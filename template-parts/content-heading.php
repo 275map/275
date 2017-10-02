1e5004e9c800ebd10de63e10467c9fcda107b0dd
miya0001 <miya@wpist.me>
2017-10-03 01:53:05 +0900

    remove header image

--- template-parts/content-heading.php
+++ template-parts/content-heading.php
@@ -8,7 +8,6 @@
     <header>
         <h1 class="the-title" role="heading">
             <?php the_title(); ?>
-			<div style="margin: 0.5em 0 1em 0;"><?php the_post_thumbnail(); ?></div>
         </h1>
     </header>
 <?php else: ?>
